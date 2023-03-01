b1f9ba4704eef0311f0078b4e7a212aaadfaa949
adityaricki <52729836+Aditya170700@users.noreply.github.com>
2023-03-01 10:12:03 +0700

    Create model

---
+++ Data/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace MyBooks.Data.Models
+{
+	public class Book
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string description { get; set; }
+		public bool IsRead { get; set; }
+		public DateTime? ReadAt { get; set; }
+		public int Rate { get; set; }
+		public string Genre { get; set; }
+		public string Author { get; set; }
+        public string CoverUrl { get; set; }
+		public DateTime CreatedAt { get; set; }
+	}
+}
+
